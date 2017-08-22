0a9e3853ee734bdbbd041f4f618fc9dad3af729b
Andrew Euredjian <andrew_103@rocketmail.com>
2017-08-22 17:09:35 -0400

    Switch filter order for DB query on item detail view

--- main/__init__.py
+++ main/__init__.py
@@ -571,7 +571,7 @@ def deleteItem(cat_name, item_name):
 @app.route('/catalog/<string:cat_name>/<string:item_name>')
 def showItemDescription(cat_name, item_name):
     cat = session.query(Category).filter_by(name=cat_name).one()
-    item = session.query(Item).filter_by(name=item_name, cat_id=cat.id).one_or_none()
+    item = session.query(Item).filter_by(cat_id=cat.id, name=item_name).one_or_none()
     if item is None:
         flash("Invalid item id")
         return redirect(url_for('showCatItems', cat_name=cat_name))
